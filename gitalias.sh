7d74e682e472c647ac03d4257b0b3dfa32f5a385
Alexey R <lexa260011@ngs.ru>
2018-02-20 21:15:56 +0000

    alias

--- gitalias.sh
+++ gitalias.sh
@@ -2,14 +2,14 @@ git config --global alias.co checkout
 git config --global alias.cm "commit -m"
 git config --global alias.st status
 git config --global alias.br branch
-git config --global alias.hist "log --pretty=format:'%cyan%h%Creset %ad | %s%d [%C(bold blue)%an%Creset] %C(cyan)%d%Creset %Cgreen(%cr)%Creset' --graph --date=short"
+git config --global alias.hist "log --pretty=format:'%c(cyan)%h%Creset %ad | %s%d [%C(bold blue)%an%Creset] %C(cyan)%d%Creset %Cgreen(%cr)%Creset' --graph --date=short"
 git config --global alias.ps push
 git config --global alias.pl pull
 git config --global alias.ft fetch
 git config --global alias.mg merge
 git config --global alias.cf config
 git config --global alias.ad add
-git config --global aliac.cl clone
+git config --global alias.cl clone
 
-git config --global credential.helper cache --timeout 60000
-#git config --global credential.helper store
+#git config --global credential.helper cache --timeout 60000
+git config --global credential.helper store
