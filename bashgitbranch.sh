ec91f3572ba371a46dd622ed3ce77d2c88c47bc9
Alexey R <alexey_r.98@ngs.ru>
2018-10-21 01:04:05 +0700

    ?

--- bashgitbranch.sh
+++ bashgitbranch.sh
@@ -9,7 +9,7 @@ echo "parse_git_branch() {
     fi
 }
 
-export PS1=\"\\[\\e[1;32m\\]\\u\[\\e[1;00m\\]@\\[\\e[1;34m\\]\\h \\W \\[\\e[1;31m\\]\\\$(parse_git_branch)\\[\\e[1;34m\\]$ \\[\\e[1;00m\\]\"" >> ~/.bashrc
+export PS1=\"\\[\\e[1;32m\\]\\u@\\h:\\[\\e[1;34m\\]\\W \\[\\e[1;33m\\][ \\@] \\[\\e[1;31m\\]\\\$(parse_git_branch)\\[\\e[1;34m\\]$ \\[\\e[1;00m\\]\"" >> ~/.bashrc
 echo "Start source."
 source ~/.bashrc
 echo "Done."
