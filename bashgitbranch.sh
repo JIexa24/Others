bd76b5690f98f6b03cc4adb8521a721d7ce14f30
Alexey R <alexey_r.98@ngs.ru>
2019-03-10 22:37:58 +0000

    -

--- bashgitbranch.sh
+++ bashgitbranch.sh
@@ -4,10 +4,12 @@ echo "parse_git_branch() {
     git_branch=\$(git branch 2>/dev/null | sed -n '/^\\*/s/^\\* //p')
     if [[ \$git_branch ]]; then
     echo \"[\$git_branch] \"
-    else 
+    else
     echo \"\"
     fi
 }
+
+alias l='ls -lahX'
 
 export PS1=\"\\[\\e[1;32m\\]\\u\[\\e[1;00m\\]@\\[\\e[1;34m\\]\\h \\W \\[\\e[1;31m\\]\\\$(parse_git_branch)\\[\\e[1;34m\\]$ \\[\\e[1;00m\\]\"" >> ~/.bashrc
 echo "Start source."
---
+++ configure_system.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo "Start writing parse."
+echo "parse_git_branch() {
+    git_branch=\$(git branch 2>/dev/null | sed -n '/^\\*/s/^\\* //p')
+    if [[ \$git_branch ]]; then
+    echo \"[\$git_branch] \"
+    else
+    echo \"\"
+    fi
+}
+
+alias l='ls -lahX'
+
+export PS1=\"\\[\\e[1;32m\\]\\u\[\\e[1;00m\\]@\\[\\e[1;34m\\]\\h \\W \[\e[1;33m\][ \@] \\[\\e[1;31m\\]\\\$(parse_git_branch)\\[\\e[1;34m\\]$ \\[\\e[1;00m\\]\"" >> ~/.bashrc
+echo "Start gitaliases"
+chmod +x ./gitalias.sh
+./gitalias.sh
+echo "Start source."
+source ~/.bashrc
+echo "Done."
