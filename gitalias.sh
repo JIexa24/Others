39cc74dfa0bb576b0a6fd925e81e61ffa0bff5b6
Alexey R <alexey_r.98@ngs.ru>
2019-03-10 22:59:44 +0700

    gitalias (git hist)

--- gitalias.sh
+++ gitalias.sh
@@ -2,7 +2,7 @@ git config --global alias.co checkout
 git config --global alias.cm "commit -m"
 git config --global alias.st status
 git config --global alias.br branch
-git config --global alias.hist "log --pretty=format:'%C(cyan)%h%Creset %ad | %s%C(cyan)%d%Creset [%C(bold blue)%an%Creset] %C(green)(%cr)%Creset' --graph --date=short --all"
+git config --global alias.hist "log --pretty=format:'%C(cyan)%h%Creset %ad | %s%C(cyan)%d%Creset [%C(bold blue)%an|%ae%Creset] %C(green)(%cr)%Creset' --graph --date=short --all"
 git config --global alias.ps push
 git config --global alias.pl pull
 git config --global alias.ft fetch
