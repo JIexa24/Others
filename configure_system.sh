28e9187e5944e956a124ca35b487bb3c5c991129
Alexey R <alexey_r.98@ngs.ru>
2019-05-23 16:16:49 +0700

    -

--- configure_system.sh
+++ configure_system.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 echo "Start cp bashrc."
-if [[ $1 == "+root" ]];
+if (( $# < 2))
 then
-sudo cp .bashrc ~root
+  echo "Usage:"
+  echo "\$1 - username for .bashrc"
+  echo "\$2 - aliases & privelegies set"
+  exit 1
 fi
-cp .bashrc ~/
-echo "Start gitaliases"
-chmod +x ./gitalias.sh
-./gitalias.sh
+cp ./.bashrc ./.bashrc.tmp
+if [[ $2 == "yes" ]];
+then
+  echo "HISTFILE=\"/history/\$USER_bash_history\"" >> ./.bashrc.tmp
+  echo "alias history='echo No '" >> ./.bashrc.tmp
+  echo "alias sh='echo No '" >> ./.bashrc.tmp
+  echo "alias passwd='echo No '" >> ./.bashrc.tmp
+  echo "alias chmod='echo No '" >> ./.bashrc.tmp
+  echo "alias chown='echo No '" >> ./.bashrc.tmp
+  echo "alias unalias='echo No '" >> ./.bashrc.tmp
+fi
+sudo cp ./.bashrc.tmp $1/.bashrc
+
 echo "Done."
