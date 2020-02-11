d90d9f1ef6bf190e740fd0cfbc73504b634e3f98
Alexey R <romanyuta@netpost.ru>
2020-02-11 10:53:18 +0700

    param

--- gitalias.sh
+++ gitalias.sh
@@ -1,15 +1,15 @@
-git config --global alias.co checkout
-git config --global alias.cm "commit -m"
-git config --global alias.st status
-git config --global alias.br branch
-git config --global alias.hist "log --pretty=format:'%C(cyan)%h%Creset %ad | %s%C(cyan)%d%Creset [%C(bold blue)%an|%ae%Creset] %C(green)(%cr)%Creset' --graph --date=local --all"
-git config --global alias.ps push
-git config --global alias.pl pull
-git config --global alias.ft fetch
-git config --global alias.mg merge
-git config --global alias.cf config
-git config --global alias.ad add
-git config --global alias.cl clone
+git config --$1 alias.co checkout
+git config --$1 alias.cm "commit -m"
+git config --$1 alias.st status
+git config --$1 alias.br branch
+git config --$1 alias.hist "log --pretty=format:'%C(cyan)%h%Creset %ad | %s%C(cyan)%d%Creset [%C(bold blue)%an|%ae%Creset] %C(green)(%cr)%Creset' --graph --date=local --all"
+git config --$1 alias.ps push
+git config --$1 alias.pl pull
+git config --$1 alias.ft fetch
+git config --$1 alias.mg merge
+git config --$1 alias.cf config
+git config --$1 alias.ad add
+git config --$1 alias.cl clone
 
-#git config --global credential.helper cache --timeout 60000
-git config --global credential.helper store
+#git config --$1 credential.helper cache --timeout 60000
+git config --$1 credential.helper store
